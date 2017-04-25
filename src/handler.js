bf3675d87cb650905742c838a73f1e857865eb00
John Costa <jcosta@execonline.com>
2017-04-25 15:11:50 -0700

    Create bitmovin sprite lambda
    This function creates a sprite for bitmovin encoded videos.  A sprite has two parts, a jpg immage that is really a bunch of thumbnails stiched together sequentially, and a vtt
    file that makes video times to locations on the image so it shows the right thumb when you scroll over the corresponding video duration time.  After creating the sprite files, the function finds their url location and uses it to upload them to the video encoding folder on S3.
    
    Tags: bitmovin, sprite, lambda

--- src/handler.js
+++ src/handler.js
@@ -1,28 +1,72 @@
-import BitmovinTransmuxingLambda from './BitmovinTransmuxingLambda';
+import BitmovinSpriteLambda from './BitmovinSpriteLambda';
 
 'use strict';
 require('dotenv').config();
 const jf = require('jsonfile');
 
 function processMessage(event) {
-  const message = event.body
+  let message = event.body;
+  message = JSON.parse(message);
   return message;
 }
 
 function entry(event, context, callback) {
-  const bit = new BitmovinTransmuxingLambda();
-  console.log('Starting new bitmovin transmuxing job');
+  const bit = new BitmovinSpriteLambda();
+  console.log('Starting new bitmovin sprite creation');
   const message = processMessage(event);
-  bit.createTransmuxes(message);
-  callback(null, 'Success!');
+  bit.start(message)
+    .then((result) => {
+      console.log('Successfully transferred sprite jpg and vtt files to S3', result);
+      let message = {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: "Successfully transferred sprite and completed execution of function, " + JSON.stringify(result)
+      }
+      callback(null, message);
+    })
+    .catch((error) =>{
+      console.log("ERROR failed to transfer sprite to S3", error);
+      let message = {
+        statusCode: 500,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: "Error, " + JSON.stringify(error)
+      }
+      callback(null, message);
+    }
+  );
 }
 
 function test() {
-  const bit = new BitmovinTransmuxingLambda();
-  console.log('Starting new bitmovin transmuxing job');
+  const bit = new BitmovinSpriteLambda();
+  console.log('Starting new bitmovin sprite creation');
   const event = jf.readFileSync('event.json');
   const message = processMessage(event);
-  bit.createTransmuxes(message);
+  bit.start(message)
+    .then((result) => {
+      console.log('Successfully transferred sprite jpg and vtt files to S3', result);
+      let message = {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: "Successfully transferred sprite and completed execution of function, " + JSON.stringify(result)
+      }
+    })
+    .catch((error) =>{
+      console.log("ERROR failed to transfer sprite to S3", error);
+      let message = {
+        statusCode: 500,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: "Error, " + JSON.stringify(error)
+      }
+    }
+  );
 }
 
 module.exports = {
